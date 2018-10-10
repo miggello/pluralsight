ffff63119b6c3907b63af1ee5f3b3a5e99cb4030
Michael Robinson <miggello1@gmail.com>
2018-10-10 11:09:26 -0400

    Added some error handling.

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -20,24 +20,28 @@ namespace Grades
             }
             set
             {
-                if (!String.IsNullOrEmpty(value))
+                if (String.IsNullOrEmpty(value))
                 {
-                    if(_name != value)
-                    {
-                        //NameChangedEventArgs args = new NameChangedEventArgs();
-                        //args.ExistingName = _name;
-                        //args.NewName = value;
-                        //NameChanged(this, args);
-                    }
-
-                    _name = value;
+                    throw new ArgumentException("Name cannot be null or empty");
                 }
+
+                if (_name != value && NameChanged != null)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+
+                    NameChanged(this, args);
+                }
+
+                _name = value;
             }
+
         }
 
         public void WriteGrades(TextWriter destination)
         {
-            for (int  i = 0;  i < grades.Count;  i++)
+            for (int i = 0; i < grades.Count; i++)
             {
 
                 destination.WriteLine(grades[i]);
@@ -70,6 +74,6 @@ namespace Grades
             grades.Add(grade);
         }
 
-        //public event NameChangedDelegate NameChanged;
+        public event NameChangedDelegate NameChanged;
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,33 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
+            Console.WriteLine("Enter a name:");
+
+            try
+            {
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
-            book.WriteGrades(Console.Out);
+
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
 
             GradeStatistics stats = book.ComputeStatistics();
             WriteResult("Average", stats.AverageGrade);
