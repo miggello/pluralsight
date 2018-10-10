463976ecf8d7375e345e2775cf5a0af4f6a4b661
Michael Robinson <miggello1@gmail.com>
2018-10-10 09:20:53 -0400

    Extended Gradebook class functionality with 'Description' and 'LetterGrade properties.

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,15 @@ namespace Grades
 
                     _name = value;
                 }
+            }
+        }
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int  i = 0;  i < grades.Count;  i++)
+            {
+
+                destination.WriteLine(grades[i]);
             }
         }
 
